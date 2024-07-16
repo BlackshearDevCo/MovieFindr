e3bb8238ded1192e67cef825a927365fed8ebe84
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-07-16 14:48:51 -0600

    Initial commit

---
+++ components/Header/SearchInput.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+
+type People = {
+  id: number;
+  name: string;
+};
+
+const people: People[] = [
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
+];
+
+export function SearchInput() {
+  const [selectedPerson, setSelectedPerson] = useState<Nullable<People>>(null);
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
+
+  return (
+    <Combobox
+      value={selectedPerson}
+      onChange={setSelectedPerson}
+      onClose={() => setQuery("")}
+    >
+      <ComboboxInput
+        aria-label="Assignee"
+        displayValue={(person: Maybe<People>) => person?.name || query}
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Search for a movie..."
+        className="px-4 py-2 rounded text-text w-80"
+      />
+      <ComboboxOptions
+        anchor="bottom"
+        portal={false}
+        className="border-2 border-secondary drop-shadow empty:invisible w-80 my-2 rounded py-2 bg-background"
+      >
+        {filteredPeople.map((person) => (
+          <ComboboxOption
+            key={person.id}
+            value={person}
+            className="data-[focus]:bg-blue-100 px-4 py-2 font-medium"
+          >
+            {person.name}
+          </ComboboxOption>
+        ))}
+      </ComboboxOptions>
+    </Combobox>
+  );
+}
---
+++ components/Header/index.tsx
@@ -0,0 +1,44 @@
+import clsx from "clsx";
+import Link, { LinkProps } from "next/link";
+import React from "react";
+import { SearchInput } from "./SearchInput";
+
+export function Header() {
+  return (
+    <header className="w-full flex justify-between items-center bg-primary text-background py-4 px-4 md:px-6">
+      <div className="flex gap-12">
+        <Link href="/" prefetch={false}>
+          <h1 className="font-extrabold uppercase text-xl tracking-tight">
+            MovieFindr
+          </h1>
+        </Link>
+
+        <nav className="flex items-center gap-6">
+          <HeaderLink href="/">Home</HeaderLink>
+          <HeaderLink href="/movies">Movies</HeaderLink>
+          <HeaderLink href="/genres">Genres</HeaderLink>
+        </nav>
+      </div>
+
+      <SearchInput />
+    </header>
+  );
+}
+
+type HeaderLinkProps = LinkProps & {
+  className?: string;
+  children: React.ReactNode;
+};
+
+function HeaderLink({ href, className, children, ...props }: HeaderLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={clsx("font-bold", className)}
+      prefetch={false}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
