924950e85657f0f081bb8ddb1a2dd2b49c73f3f3
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-07-17 09:06:01 -0600

    Add Suspense to wrap movies list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { MoviesList } from "@/components/MoviesList";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -29,7 +30,9 @@ export default function Home() {
         </div>
       </section>
 
-      <MoviesList />
+      <Suspense>
+        <MoviesList />
+      </Suspense>
     </main>
   );
 }
