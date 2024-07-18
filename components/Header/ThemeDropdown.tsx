fac570772bb1c19d06874cd581d6c63a7b9a5796
Brooklyn Blackshear <ablackshear7820@gmail.com>
2024-07-17 18:01:40 -0600

    Updated theme manger dropdown icon

--- components/Header/ThemeDropdown.tsx
+++ components/Header/ThemeDropdown.tsx
@@ -2,13 +2,17 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import React, { forwardRef, useContext } from "react";
 import { ThemeContext, Types } from "@/components/ThemeProvider";
 import clsx from "clsx";
+import { SunIcon } from "../SunIcon";
 
 export function ThemeDropdown() {
   const { state, dispatch } = useContext(ThemeContext);
 
   return (
     <Menu>
-      <MenuButton>Theme switcher</MenuButton>
+      <MenuButton>
+        <SunIcon />
+        <p className="sr-only">Manage theme</p>
+      </MenuButton>
       <MenuItems
         anchor="bottom end"
         className="z-50 border-2 border-secondary drop-shadow empty:invisible [--anchor-gap:4px] w-40 rounded bg-background"
